bb485a85346db8d97ce2f3c5022f41d133f10eac
lucasmpimentel <lucas.mirandapimentel@gmail.com>
2022-01-06 20:44:26 -0300

    Requisito 3

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  const { species } = data;
-  return species.filter((item) => ids.includes(item.id));
+  return data.species.filter((specie) => ids.includes(specie.id));
 }
 
 module.exports = getSpeciesByIds;
