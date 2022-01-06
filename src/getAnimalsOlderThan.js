4f4de1a5672cbd640af482e9825cd05ff2e91b9b
lucasmpimentel <lucas.mirandapimentel@gmail.com>
2022-01-06 17:56:03 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  //
+  const { species } = data;
+  const sel = species.find((specie) => specie.name === animal);
+  return sel.residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,9 +2,6 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   const { species } = data;
-  if (ids === undefined) {
-    return [];
-  }
   return species.filter((item) => ids.includes(item.id));
 }
 
