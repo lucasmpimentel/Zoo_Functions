077a39a8732a684b0a315d0116e20889e8f67509
lucasmpimentel <lucas.mirandapimentel@gmail.com>
2022-01-07 20:11:46 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const animals = species.reduce((acc, curr) => {
+      // console.log(curr.residents.length);
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+    return animals;
+  }
+  const quantAnimal = species.find((quant) => quant.name === animal.specie).residents;
+  //Fonte Object.keys https://stackoverflow.com/questions/5223/length-of-a-javascript-object
+  if (Object.keys(animal).length === 1) {
+    return quantAnimal.length;
+  }
+  const sexAnimal = quantAnimal.filter((elem) => elem.sex === animal.sex);
+  return sexAnimal.length;
 }
 
 module.exports = countAnimals;
