180408697268b9d8219f29244e28549eebc4120b
lucasmpimentel <lucas.mirandapimentel@gmail.com>
2022-01-06 23:24:02 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
+const { employees: e } = data;
+
 function isManager(id) {
-  // seu código aqui
+  const ver = e.find((man) => man.managers.includes(id));
+  if (typeof ver !== 'undefined') {
+    return true;
+  }
+  return false;
 }
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
+function getRelatedEmployees(id) {
+  if (!isManager(id)) throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+
+  const names = e.filter((emp) => emp.managers.includes(id));
+  return names.map((emp) => `${emp.firstName} ${emp.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
