bcfc8ebdc13b5420f406ed9ffecb28d9adc4c1d5
lucasmpimentel <lucas.mirandapimentel@gmail.com>
2022-01-10 19:19:10 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const curEmp = employees.find((employee) => employee.id === id);
+  const curSpecieId = curEmp.responsibleFor[0];
+  const { residents } = species.find((specie) => specie.id === curSpecieId);
+  const old = residents.reduce((acc, resident) => (resident.age > acc.age ? resident : acc));
+  return Object.values(old);
 }
 
 module.exports = getOldestFromFirstSpecies;
