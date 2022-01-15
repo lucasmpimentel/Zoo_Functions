a18b06be247d699e9610933a30a0a9df3c604da2
lucasmpimentel <lucas.mirandapimentel@gmail.com>
2022-01-15 20:32:58 -0300

    Requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,46 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees, species } = data;
+
+const emp = employees.map((employee) => {
+  const { id, firstName, lastName, responsibleFor } = employee;
+  const getSpec = species.filter((animal) => responsibleFor.includes(animal.id));
+  return {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: getSpec.map((animal) => animal.name),
+    locations: getSpec.map((animal) => animal.location),
+  };
+});
+let getEmployee;
+
+function validation(input) {
+  return employees.reduce((_, employee) => {
+    const { id, firstName, lastName, responsibleFor } = employee;
+    const getSpec = species.filter((animal) => responsibleFor.includes(animal.id));
+    if (input === id || input === firstName || input === lastName) {
+      getEmployee = {
+        id,
+        fullName: `${firstName} ${lastName}`,
+        species: getSpec.map((animal) => animal.name),
+        locations: getSpec.map((animal) => animal.location),
+      };
+    }
+    return getEmployee;
+  }, {});
+}
+
+function getEmployeesCoverage(search) {
+  if (!search) return emp;
+  const input = Object.values(search)[0];
+  const result = validation(input);
+  if (result === undefined) {
+    throw new Error('Informações inválidas');
+  } else {
+    return result;
+  }
 }
 
 module.exports = getEmployeesCoverage;
+
+// getEmployeesCoverage({ name: 'Sharoanda' });
